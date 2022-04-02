acc96a4f82b63a3e922ba3114e9be6f03f895836
Chaochao <chaochao.zhou@hotmail.com>
2022-04-02 19:11:58 +0200

    Connectivity Status

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,9 @@ import component from "./utils/component";
 
 import draw from "./components/Draw";
 
+import connectivity from "./components/Connectivity";
+
 await store.commits.deck.generateDeck();
 
 component(draw, document.getElementById('draw'));
+component(connectivity, document.getElementById('connectivity'));
